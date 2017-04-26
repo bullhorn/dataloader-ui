bf357664d74697909fc198dcea26dac3766b2115
Nathan Dickerson <nathan@edickerson.net>
2017-04-26 07:19:57 -0500

    Filled out the rest of the settings form elements

--- public/components/settings/Settings.js
+++ public/components/settings/Settings.js
@@ -1,5 +1,5 @@
-import {Component, OnInit, FormGroup, FormControl} from '@angular/core';
-import {FormUtils, TextBoxControl} from 'novo-elements';
+import { Component, OnInit } from '@angular/core';
+import { FormUtils, TextBoxControl, TilesControl } from 'novo-elements';
 
 @Component({
     selector: 'settings',
@@ -43,10 +43,52 @@ export class Settings implements OnInit {
                 value: '',
                 required: true,
                 key: 'clientSecret'
+            }),
+            dataCenterControl: new TilesControl({
+                key: 'dataCenter',
+                label: 'Data Center',
+                value: 'east',
+                required: false,
+                options: [
+                    {
+                        label: 'US-East',
+                        value: 'east'
+                    },
+                    {
+                        label: 'US-West',
+                        value: 'west'
+                    },
+                    {
+                        label: 'US-BHNext',
+                        value: 'bhnext'
+                    },
+                    {
+                        label: 'UK',
+                        value: 'uk'
+                    },
+                    {
+                        label: 'Other',
+                        value: 'other'
+                    }
+                ]
+            }),
+            listDelimiterControl: new TextBoxControl({
+                type: 'text',
+                label: 'List Delimiter',
+                value: ';',
+                required: true,
+                key: 'listDelimiter'
+            }),
+            dateFormatControl: new TextBoxControl({
+                type: 'text',
+                label: 'Date Format',
+                value: 'MM/dd/yy HH:mm',
+                required: true,
+                key: 'dateFormat'
             })
         };
 
-        this.form = this.formUtils.toFormGroup([this.formControls.usernameControl, this.formControls.passwordControl, this.formControls.clientIdControl, this.formControls.clientSecretControl]);
+        this.form = this.formUtils.toFormGroup([this.formControls.usernameControl, this.formControls.passwordControl, this.formControls.clientIdControl, this.formControls.clientSecretControl, this.formControls.dataCenterControl, this.formControls.listDelimiterControl, this.formControls.dateFormatControl]);
     }
 
     save() {
