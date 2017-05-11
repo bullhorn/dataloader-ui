cd195adea97f60c960279bb8999fc4639c53fff3
Elizabeth Carretto <elizabeth.carretto@bullhorn.com>
2017-05-10 22:26:57 -0500

    WIP kicking of routes and rearranging

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ let win;
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({ width: 1200, height: 900 });
+    win = new BrowserWindow({ width: 1200, height: 780 });
 
     // and load the index.html of the app.
     win.loadURL(url.format({
--- public/dataloader-ui.module.js
+++ public/dataloader-ui.module.js
@@ -2,13 +2,23 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import {APP_BASE_HREF} from '@angular/common';
+import { Routes, RouterModule, PreloadAllModules, ActivatedRouteSnapshot } from '@angular/router';
+
 // Vendor
 import { NovoElementsModule, NovoElementProviders, FormUtils } from 'novo-elements';
 // APP
 import { DataloaderUI } from './app/DataloaderUI';
 import './dataloader-ui.module.scss';
-import { ComponentsModule } from './components/components.module.js';
+import { ComponentsModule } from './components/components.module';
+import { Settings, Load } from './components/all';
+
+export const routes = [
+    { path: '', redirectTo: '/load', pathMatch: 'full' },
+    { path: 'load', component: Load },
+    { path: 'settings', component: Settings },
+];
 
 @NgModule({
     declarations: [
@@ -21,9 +31,11 @@ import { ComponentsModule } from './components/components.module.js';
         HttpModule,
         NovoElementsModule,
         NovoElementProviders.forRoot(),
-        ComponentsModule
+        ComponentsModule,
+        RouterModule.forRoot(routes, { useHash: true })
     ],
     providers: [
+        { provide: APP_BASE_HREF, useValue : '' },
         FormUtils
     ],
     entryComponents: [
