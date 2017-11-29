54fb4b1fe74fea2249c1532c5353103e41a186a7
Nathan Dickerson <ndickerson@bullhorn.com>
2017-11-29 08:15:54 -0600

    Refactored fakes out to their own file

---
+++ src/app/providers/file/file.service.fakes.ts
@@ -0,0 +1,77 @@
+// App
+import { IPreviewData } from '../../../interfaces/IPreviewData';
+import { ISettings } from '../../../interfaces/ISettings';
+import { IResults } from '../../../interfaces/IResults';
+import Timer = NodeJS.Timer;
+
+/**
+ * Fake test data for running in `ng serve` mode
+ */
+export class FileServiceFakes {
+
+  public static SETTINGS: ISettings = {
+    username: 'jsmith',
+    password: 'password!',
+    clientId: '12345',
+    clientSecret: '67890',
+    dataCenter: 'west',
+    authorizeUrl: '',
+    tokenUrl: '',
+    loginUrl: '',
+    listDelimiter: ';',
+    dateFormat: 'MM/dd/yyyy',
+    numThreads: 15,
+  };
+
+  public static PREVIEW_DATA: IPreviewData = {
+    total: 210,
+    headers: ['firstName', 'lastName', 'email'],
+    data: [{
+      firstName: 'John',
+      lastName: 'Smith',
+      email: 'jsmith@example.com',
+    }, {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'jdoe@example.com',
+    }, {
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: 'jdoe@example.com',
+    }],
+  };
+
+  public static generateFakeResults(callback: (results: IResults) => {}): void {
+    let fakeResults: IResults = {
+      processed: 0,
+      inserted: 0,
+      updated: 0,
+      deleted: 0,
+      failed: 0,
+      successFile: '/Path/to/dataloader/results/Candidate_load_success.csv',
+      failureFile: '/Path/to/dataloader/results/Candidate_load_failure.csv',
+      logFile: '/Path/to/dataloader/log/dataloader_2017-11-20_08.22.21.log',
+      startTime: 1511182001000,
+      durationMsec: 0,
+      errors: [],
+    };
+    const MAX_ITERATIONS: number = 30;
+    let i: number = 0;
+    let interval: Timer = setInterval(() => {
+      fakeResults.processed += 7;
+      fakeResults.inserted += 4;
+      fakeResults.updated += 2;
+      fakeResults.failed += 1;
+      fakeResults.durationMsec += 1000;
+      fakeResults.errors.push({
+        row: fakeResults.failed,
+        id: fakeResults.failed + 4,
+        message: 'com.bullhornsdk.data.exception.RestApiException: Cannot find To-One Association: \'owner.name\' with value: \'Bogus\''
+      });
+      callback(fakeResults);
+      if (++i >= MAX_ITERATIONS) {
+        clearInterval(interval);
+      }
+    }, 500);
+  }
+}
--- src/app/providers/file/file.service.ts
+++ src/app/providers/file/file.service.ts
@@ -2,9 +2,10 @@
 import { Injectable } from '@angular/core';
 // App
 import { ElectronService } from '../electron/electron.service';
+import { FileServiceFakes } from './file.service.fakes';
 import { IPreviewData } from '../../../interfaces/IPreviewData';
-import { ISettings } from '../../../interfaces/ISettings';
 import { IResults } from '../../../interfaces/IResults';
+import { ISettings } from '../../../interfaces/ISettings';
 
 @Injectable()
 export class FileService {
@@ -16,26 +17,11 @@ export class FileService {
   }
 
   readSettings(): ISettings {
-    let settings: ISettings;
     if (ElectronService.isElectron()) {
-      settings = JSON.parse(this.electronService.fs.readFileSync(FileService.SETTINGS_FILE, 'utf8'));
+      return JSON.parse(this.electronService.fs.readFileSync(FileService.SETTINGS_FILE, 'utf8'));
     } else {
-      // Call with fake test data for running in `ng serve` mode
-      settings = {
-        username: 'jsmith',
-        password: 'password!',
-        clientId: '12345',
-        clientSecret: '67890',
-        dataCenter: 'west',
-        authorizeUrl: '',
-        tokenUrl: '',
-        loginUrl: '',
-        listDelimiter: ';',
-        dateFormat: 'MM/dd/yyyy',
-        numThreads: 15,
-      };
+      return FileServiceFakes.SETTINGS;
     }
-    return settings;
   }
 
   writeSettings(value: ISettings): void {
@@ -92,24 +78,7 @@ export class FileService {
           console.error(error); // tslint:disable-line:no-console
         });
     } else {
-      // Call with fake test data for running in `ng serve` mode
-      onSuccess({
-        total: 1131,
-        headers: ['firstName', 'lastName', 'email'],
-        data: [{
-          firstName: 'John',
-          lastName: 'Smith',
-          email: 'jsmith@example.com',
-        }, {
-          firstName: 'John',
-          lastName: 'Doe',
-          email: 'jdoe@example.com',
-        }, {
-          firstName: 'Jane',
-          lastName: 'Doe',
-          email: 'jdoe@example.com',
-        }],
-      });
+      onSuccess(FileServiceFakes.PREVIEW_DATA);
     }
   }
 
@@ -119,39 +88,12 @@ export class FileService {
     }
   }
 
-  // TODO: Move fakes out to STATIC file.service.fakes.ts
   onResultsFileChange(onChange: (results: IResults) => {}): void {
     if (ElectronService.isElectron()) {
       let options = { persistent: true, interval: 500 };
       this.electronService.fs.watchFile(FileService.RESULTS_FILE, options, this.readResultsFile.bind(this, onChange));
     } else {
-      // Call with fake test data for running in `ng serve` mode
-      let fakeResults: IResults = {
-        processed: 0,
-        inserted: 0,
-        updated: 0,
-        deleted: 0,
-        failed: 0,
-        successFile: '/Users/nathandickerson/Source/dataloader/results/Candidate_load_2017-11-21_07.20.06_success.csv',
-        failureFile: '/Users/nathandickerson/Source/dataloader/results/Candidate_load_2017-11-21_07.20.06_failure.csv',
-        logFile: '/Users/nathandickerson/Source/dataloader/log/dataloader_2017-11-20_08.22.21.log',
-        startTime: 1511182001000,
-        durationMsec: 0,
-        errors: [],
-      };
-      setInterval(() => {
-        fakeResults.processed += 6;
-        fakeResults.inserted += 3;
-        fakeResults.updated += 2;
-        fakeResults.failed += 1;
-        fakeResults.durationMsec += 1000;
-        fakeResults.errors.push({
-          row: fakeResults.failed,
-          id: fakeResults.failed + 111,
-          message: 'com.bullhornsdk.data.exception.RestApiException: Cannot find To-One Association: \'owner.name\' with value: \'Bogus\''
-        });
-        onChange(fakeResults);
-      }, 500);
+      FileServiceFakes.generateFakeResults(onChange);
     }
   }
 
