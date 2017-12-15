1ac8622594fb805912dca01d24cc3b2c6bd14285
Chris Erxleben <christopher.erxleben@bullhorn.com>
2017-12-14 21:33:41 -0600

    #14 - Change to make navbar fixed to the top of the window

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,15 @@
 // Angular
 import { RouterModule, Routes } from '@angular/router';
 // App
-import { AppComponent } from './app.component';
 import { LoadComponent } from './components/load/load.component';
 import { ResultsComponent } from './components/results/results.component';
 import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-    children: [
-      { path: '', redirectTo: 'load', pathMatch: 'full' },
-      { path: 'load', component: LoadComponent },
-      { path: 'results', component: ResultsComponent },
-      { path: 'settings', component: SettingsComponent },
-    ],
-  },
+  { path: '', redirectTo: 'load', pathMatch: 'full' },
+  { path: 'load', component: LoadComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: 'settings', component: SettingsComponent },
 ];
 
 export const routing: any = RouterModule.forRoot(routes);
