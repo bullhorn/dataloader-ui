a43884fce50c904fc1620723ffec231c3e372e7f
Nathan Dickerson <ndickerson@bullhorn.com>
2018-01-02 08:27:53 -0600

    Smarter download of CLI - only if there is a newer version

--- download-latest-dataloader.js
+++ download-latest-dataloader.js
@@ -1,6 +1,7 @@
-let request = require('request');
 let fs = require('fs');
+let request = require('request');
 let extract = require('extract-zip');
+let rimraf = require('rimraf');
 let log = console.log;
 
 const TOKEN = '47728eb9ca3a3043ac064927cd8cb51c35f260a2';
@@ -15,9 +16,18 @@ let latestReleaseAssets = {
 };
 
 request(latestReleaseAssets, (error, response, bodyString) => {
-  error ? log(error) : log('downloading...');
+  error ? log(error) : log('checking version...');
 
   let body = JSON.parse(bodyString);
+  let version = body.tag_name.slice(1); // `v1.2.3` minus the `v`
+  if (fs.existsSync(`dataloader/dataloader-${version}.jar`)) {
+    log(`dataloader version: ${version} is up to date.`);
+    process.exit();
+  } else {
+    log(`downloading newer version: ${version}...`);
+    rimraf.sync('./dataloader');
+  }
+
   let assetID = body.assets.find((asset) => asset.name === FILE).id;
   let downloadDataLoaderZip = {
     url: `${BASE_URL}/releases/assets/${assetID}`,
