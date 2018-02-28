64da072023930d481b756111e6ba2d5aed536227
Chris Erxleben <christopher.erxleben@bullhorn.com>
2018-02-28 12:48:02 -0600

    WIP: updated abbreviated filter logic

--- src/app/pipes/abbreviated-number/abbreviated-number.pipe.ts
+++ src/app/pipes/abbreviated-number/abbreviated-number.pipe.ts
@@ -1,19 +1,45 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 /*
- * Transform a number to it's abbreviated notation
+ * Transform a number to it's abbreviated notation (without rounding)
+ * Usage:
+ *   num | abbreviatedNumber
+ * Example:
+ *   {{199 | abbreviatedNotation}}
+ *   formats to: 199
+ * Example:
+ *   {{1200 | abbreviatedNotation}}
+ *   formats to: 1.2k
+ * Example:
+ *   {{125000000 | abbreviatedNotation}}
+ *   formats to: 125.0m
  */
 @Pipe({ name: 'abbreviatedNumber' })
 export class AbbreviatedNumberPipe implements PipeTransform {
   transform(num: number): string|number {
     if (num < 1000) {
       return num;
-    } else if (num < 1000000) {
-      return num / 1000 + 'k';
-    } else if (num < 1000000000) {
-      return num / 1000000 + 'm';
     } else {
-      return num / 1000000000 + 'b';
+      let d: number|undefined;
+      let letter: string = '';
+      if (num < 1000000) {
+        d = 1000;
+        letter = 'k';
+      } else if (num < 1000000000) {
+        d = 1000000;
+        letter = 'm';
+      } else {
+        d = 1000000000;
+        letter = 'b';
+      }
+      num /= d;
+      if (Number.isInteger(num)) {
+        return `${num}.0${letter}`;
+      } else {
+        const numStr: string = num.toString();
+        const substrTo: number = numStr.indexOf('.') + 2;
+        return `${numStr.substr(0, substrTo)}${letter}`;
+      }
     }
   }
 }
