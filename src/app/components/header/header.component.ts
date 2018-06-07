673672f92fbec07b917e282703a7d3228256a90c
Nathan Dickerson <nathan@edickerson.net>
2018-06-07 15:15:46 -0500

    feat(Usability): Added open link for CSV file (#44)
    * Added a nice side-panel shadow
    
    * Better shadow
    
    * Added CSV Input File Link
    
    * Opening settings upon startup if credentials are missing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
 // Angular
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 // Vendor
 import { NovoModalService } from 'novo-elements';
 // App
+import { FileService } from '../../providers/file/file.service';
+import { ISettings } from '../../../interfaces/ISettings';
 import { SettingsComponent } from '../settings/settings.component';
 
 @Component({
@@ -10,8 +12,17 @@ import { SettingsComponent } from '../settings/settings.component';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private modalService: NovoModalService) {}
+export class HeaderComponent implements OnInit {
+  constructor(private modalService: NovoModalService,
+              private fileService: FileService) {
+  }
+
+  ngOnInit(): void {
+    const settings: ISettings = this.fileService.readSettings();
+    if (!settings.username || !settings.password || !settings.clientId || !settings.clientSecret) {
+      this.openSettingsModal();
+    }
+  }
 
   openSettingsModal(): void {
     this.modalService.open(SettingsComponent);
