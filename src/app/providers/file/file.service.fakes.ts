20021fc68779a1123a07f260ab15ad4c136f799f
Nathan Dickerson <ndickerson@bullhorn.com>
2018-12-07 14:56:35 -0600

    feat(Settings): Added Wildcard Search setting

--- src/app/providers/file/file.service.fakes.ts
+++ src/app/providers/file/file.service.fakes.ts
@@ -82,6 +82,7 @@ export class FileServiceFakes {
     listDelimiter: ';',
     dateFormat: 'MM/dd/yyyy',
     processEmptyAssociations: false,
+    wildcardMatching: true,
     singleByteEncoding: false,
     numThreads: 15,
     existFields: [{
