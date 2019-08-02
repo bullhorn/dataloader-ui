110f9c613a3f8d0a19cab79b2d6d1f11cb3856e4
Nathan Dickerson <ndickerson@bullhorn.com>
2019-08-02 12:36:25 -0500

    chore(Deps): Upgrading to Angular 7 / Node 10 - Step 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,3 +6,11 @@
 export const environment: any = {
   production: false,
 };
+
+/*
+ * In development mode, for easier debugging, you can ignore zone related error
+ * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
+ * below file. Don't forget to comment it out in production mode
+ * because it will have a performance impact when errors are thrown
+ */
+import 'zone.js/dist/zone-error'; // Included with Angular CLI.
