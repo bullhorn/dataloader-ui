f67f4ed8da1711d82f9ab7bdbb1d45c1ce658aa9
Nathan Dickerson <ndickerson@bullhorn.com>
2020-02-03 13:13:37 -0600

    fix(About): Updated about modal

--- src/app/components/about-modal/about-modal.component.ts
+++ src/app/components/about-modal/about-modal.component.ts
@@ -2,16 +2,24 @@
 import { Component } from '@angular/core';
 // Vendor
 import { NovoModalRef } from 'novo-elements';
+import { ElectronService } from '../../services/electron/electron.service';
 
 @Component({
   selector: 'app-about-modal',
   templateUrl: './about-modal.component.html',
 })
 export class AboutModalComponent {
-  constructor(private modalRef: NovoModalRef) {
+  constructor(private electronService: ElectronService,
+              private modalRef: NovoModalRef) {
   }
 
   close(): void {
     this.modalRef.close();
+  }
+
+  openLink(link: string): void {
+    if (ElectronService.isElectron()) {
+      this.electronService.shell.openExternal(link);
+    }
   }
 }
